d5ee5e840cf7e09533d22d18f11caacbe315acd8
MarceloRemiro <mrf0028@alu.medac.es>
2026-04-24 14:36:16 +0200

    feat: integrate Firebase core into the project

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:hair_connect/core/theme/app_colors.dart';
 import 'package:hair_connect/core/theme/app_theme.dart';
 import 'package:hair_connect/features/auth/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:hair_connect/firebase_options.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
